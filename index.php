135ae9f22174933e4f2b6b24406fdac303ee631b
Abdullah Akasha <121800837+AbdullahAkasha@users.noreply.github.com>
2024-09-18 07:11:22 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,214 @@
+<?php
+include('includes/connect.php');
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" 
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Ecommerce Website</title>
+  </head>
+  <body>
+    <!-- Navbar  -->
+
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <img src="./images/logo-1.png" alt="Logo" class="logo">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>0</sup></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Total Price</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<!-- Second Child -->
+ <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <ul class="navbar-nav me-auto">
+    <li class="nav-item">
+        <a class="nav-link" href="#">Welcome Guest</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+  </ul>
+ </nav>
+ <!-- Third Child -->
+  <div class="bg-light">
+    <h3 class="text-center">Clothing Store</h3>
+    <p class="text-center">Communication is at the heart of Ecommerce and Community.</p>
+  </div>
+  <!-- Fourth Child -->
+   <div class="row">
+    <div class="col-md-10">
+      <!-- All Products -->
+       <div class="row">
+        <div class="col-md-4 mb-2">
+          <div class="card">
+  <img src="./images/product-1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2">
+          <div class="card">
+  <img src="./images/product-2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2">
+          <div class="card">
+  <img src="./images/product-3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>    
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2">
+          <div class="card">
+  <img src="./images/product-3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>    
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2">
+          <div class="card">
+  <img src="./images/product-1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>    
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2">
+          <div class="card">
+  <img src="./images/product-2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>    
+  </div>
+</div>
+        </div>
+       </div>
+    </div>
+    <div class="col-md-2 bg-secondary p-0">
+      <!-- Side nav -->
+       <!-- Brands -->
+       <ul class="navbar-nav me-auto text-center">
+        <li class="nav-item bg-info">
+          <a href="#" class="nav-link text-light"><h4>Brands</h4></a>
+        </li>
+        <?php
+        $select_brands = "select * from brands";
+        $run_brands = mysqli_query($conn, $select_brands);
+        while($row_brands = mysqli_fetch_array($run_brands)){
+            $brand_title = $row_brands['brand_title'];
+            $brand_id = $row_brands['brand_id'];
+            echo "
+            <li class='nav-item'>
+          <a href='index.php?brand=$brand_id' class='nav-link text-light'>$brand_title</a>
+        </li>
+            ";
+        }
+        
+        ?>
+       </ul>
+       <!-- Categories -->
+               <ul class="navbar-nav me-auto text-center">
+        <li class="nav-item bg-info">
+          <a href="#" class="nav-link text-light"><h4>Caategoriess</h4></a>
+        </li>
+        <?php
+        $category_query = "select * from categories";
+        $run_query = mysqli_query($conn, $category_query);
+        while($row = mysqli_fetch_array($run_query)){
+            $category_title = $row['category_title'];
+            $category_id = $row['category_id'];
+            echo "
+            <li class='nav-item'>
+          <a href='index.php?category=$category_id' class='nav-link text-light'>$category_title</a>
+        </li>
+            ";
+        }
+        ?>
+       </ul>
+    </div>
+   </div>
+
+
+<!-- Footer -->
+
+    <div class="bg-info p-3 text-center">
+        <p>All Copy Right Reserved!</p>
+    </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
